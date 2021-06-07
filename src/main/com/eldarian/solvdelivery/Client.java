66588b577bcb217a846f748197f22f46f5e9f87f
Dmitry Plyshevsky <eldarian3@gmail.com>
2021-06-07 11:11:50 +0300

    refactor: Switch-case statement in operator choosing, minor changes in requests

--- src/main/com/eldarian/solvdelivery/Client.java
+++ src/main/com/eldarian/solvdelivery/Client.java
@@ -13,24 +13,34 @@ public class Client {
         Operator operator = null;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to DeliereD! [p]hone or [w]eb?");
-        String line = scanner.nextLine();
-
         do {
-            if(line.equals("p")) {
-                operator = manager.getPhoneOperator();
-            } else if (line.equals("w")) {
-                operator = manager.getWebOperator();
+            String line = scanner.nextLine();
+            switch (line) {
+                case "phone":
+                case "p":
+                    operator = manager.getPhoneOperator();
+                    System.out.println("Called Phone Operator");
+                    break;
+                case "web":
+                case "w":
+                    operator = manager.getWebOperator();
+                    System.out.println("Called Web Operator");
+                    break;
+                default:
+                    System.out.println("Incorrect type, try again. [p]hone or [w]eb?");
+                    break;
             }
         } while (operator == null);
 
-        System.out.println("Enter your address (Letter for street, integer for number");
-        operator.handleAddress(scanner.nextLine());
-        System.out.println("Choose cafe:");
+
+        System.out.println("Choose cafe, type its name or building number:");
         operator.handleRestaurant(scanner.nextLine());
-        System.out.println("Make an order:");
+        System.out.println("Choose your dish:");
         operator.handleDish(scanner.nextInt());
         System.out.println("Enter client data (phone num for phone client, login for web:");
         operator.handleClientData(scanner.nextLine());
+        System.out.println("Enter your address (Building number)");
+        operator.handleAddress(scanner.nextLine());
         System.out.println("Confirm your order:");
         operator.confirmOrder(scanner.nextLine());
     }
