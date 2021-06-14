6ec36acd43dce51c467ecc2d04d1aaef4064152e
Dmitry Plyshevsky <eldarian3@gmail.com>
2021-06-14 10:48:15 +0300

    refactor: cleaned executor, created controller class

--- src/main/com/eldarian/solvdelivery/Executor.java
+++ src/main/com/eldarian/solvdelivery/Executor.java
@@ -4,15 +4,17 @@ import com.eldarian.solvdelivery.city.Street;
 import com.eldarian.solvdelivery.data.Database;
 import com.eldarian.solvdelivery.ordering.Client;
 import com.eldarian.solvdelivery.staff.Manager;
+import com.eldarian.solvdelivery.staff.contact.Operator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Executor {
 
 	public static void main(String[] args) {
-		Database db = new Database();
-		Manager manager = db.getManager();
 		Client client = new Client();
 
-		client.sendOrder(manager);
+		client.sendOrder();
 	}
 
 }
--- src/main/com/eldarian/solvdelivery/ordering/Client.java
+++ src/main/com/eldarian/solvdelivery/ordering/Client.java
@@ -1,11 +1,13 @@
 package com.eldarian.solvdelivery.ordering;
 
+import com.eldarian.solvdelivery.data.OrderController;
 import com.eldarian.solvdelivery.staff.Manager;
 import com.eldarian.solvdelivery.staff.contact.Operator;
 
 public class Client {
 
-    public void sendOrder(Manager manager) {
+    public void sendOrder() {
+        Manager manager = OrderController.getInstance().getManager();
         Operator operator = manager.contactOperator();
         if(operator == null) return;
         Order order = operator.generateOrder();
